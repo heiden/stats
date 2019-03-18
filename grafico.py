3abf15c5bbf4b36a60080e291f4d2bbd43e91bae
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-03-18 15:45:55 -0300

    path/to/file

--- grafico.py
+++ grafico.py
@@ -7,35 +7,35 @@ ax.set_title(titulo)
 ax.set_xlabel('risco')
 ax.set_ylabel('retorno')
 
-with open('res-nsga-literatura-15') as arq:
+with open('./data/res-nsga-literatura-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#bb4444', label = 'nsga literatura')
 
-with open('res-brkga-15') as arq:
+with open('./data/res-brkga-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#fe12fe', label = 'brkga')
 
-with open('res-brkga-nsga-15') as arq:
+with open('./data/res-brkga-nsga-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#5f5f5f', label = 'brkga+nsga')
 
-with open('res-meu-nsga-15') as arq:
+with open('./data/res-meu-nsga-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#1651af', label = 'meu nsga')
 
-with open('res-meu-nsga-adap-15') as arq:
+with open('./data/res-meu-nsga-adap-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
