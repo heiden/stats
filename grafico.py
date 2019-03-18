bcc111663cc9431ea4a3e1a68c4a310bebb7be58
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-03-18 13:59:56 -0300

    gráficos da fronteira

--- grafico.py
+++ grafico.py
@@ -7,40 +7,40 @@ ax.set_title(titulo)
 ax.set_xlabel('risco')
 ax.set_ylabel('retorno')
 
-with open('res-nsga-literatura-3') as arq:
+with open('res-nsga-literatura-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#bb4444', label = 'nsga literatura')
 
-# with open('res-brkga-3') as arq:
-# 	linhas = arq.readlines()
-# 	x = [float(linha.split()[0]) for linha in linhas]
-# 	y = [float(linha.split()[1]) for linha in linhas]
+with open('res-brkga-15') as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
 
-# ax.plot(x, y, 'x', c = '#fe12fe', label = 'brkga')
+ax.plot(x, y, 'x', c = '#fe12fe', label = 'brkga')
 
-with open('res-brkga-nsga-3') as arq:
+with open('res-brkga-nsga-15') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#5f5f5f', label = 'brkga+nsga')
 
-# with open('res-meu-nsga-15') as arq:
-# 	linhas = arq.readlines()
-# 	x = [float(linha.split()[0]) for linha in linhas]
-# 	y = [float(linha.split()[1]) for linha in linhas]
+with open('res-meu-nsga-15') as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
 
-# ax.plot(x, y, 'x', c = '#1651af', label = 'meu nsga')
+ax.plot(x, y, 'x', c = '#1651af', label = 'meu nsga')
 
-# with open('res-meu-nsga-adap-15') as arq:
-# 	linhas = arq.readlines()
-# 	x = [float(linha.split()[0]) for linha in linhas]
-# 	y = [float(linha.split()[1]) for linha in linhas]
+with open('res-meu-nsga-adap-15') as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
 
-# ax.plot(x, y, 'x', c = '#cbacbf', label = 'meu nsga adap')
+ax.plot(x, y, 'x', c = '#cbacbf', label = 'meu nsga adap')
 
 leg = ax.legend()
 
