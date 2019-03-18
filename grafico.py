cddd2075084b2bd2ad2af3395de3694ee9f2ef4c
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-03-18 10:49:54 -0300

    testes // dados das execuções

--- grafico.py
+++ grafico.py
@@ -2,45 +2,45 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-titulo = 'portfolios'
+titulo = 'portfolios-15'
 ax.set_title(titulo)
 ax.set_xlabel('risco')
 ax.set_ylabel('retorno')
 
-with open('res-nsga-literatura') as arq:
+with open('res-nsga-literatura-3') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#bb4444', label = 'nsga literatura')
 
-with open('res-brkga') as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
+# with open('res-brkga-3') as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#fe12fe', label = 'brkga')
+# ax.plot(x, y, 'x', c = '#fe12fe', label = 'brkga')
 
-with open('res-brkga-nsga') as arq:
+with open('res-brkga-nsga-3') as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#5f5f5f', label = 'brkga+nsga')
 
-with open('res-meu-nsga') as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
+# with open('res-meu-nsga-15') as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#1651af', label = 'meu nsga')
+# ax.plot(x, y, 'x', c = '#1651af', label = 'meu nsga')
 
-with open('res-meu-nsga-adap') as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
+# with open('res-meu-nsga-adap-15') as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#cbacbf', label = 'meu nsga adap')
+# ax.plot(x, y, 'x', c = '#cbacbf', label = 'meu nsga adap')
 
 leg = ax.legend()
 
