723d8f2aacb074487d72cbc5bbc736aadd6a42ce
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-03-22 19:47:44 -0300

    whatever

--- fronteiras/grafico.py
+++ fronteiras/grafico.py
@@ -11,28 +11,28 @@ ax.set_xlabel('Risco')
 ax.set_ylabel('Retorno')
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
-with open('./res/res-brkga-' + n) as arq:
+with open('./res/' + n + '/res-brkga-' + n) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, '^', c = '#000000', label = 'BRKGA')
 
-with open('./res/res-brknsga-' + n) as arq:
+with open('./res/' + n + '/res-brknsga-' + n) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, '.', c = '#ABB7B7', label = 'BRKNSGA')
 
-with open('./res/res-nsgam-' + n) as arq:
+with open('./res/' + n + '/res-nsgam-' + n) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, '*', c = '#6C7A89', label = 'NSGA-M')
 
-with open('./res/res-nsgal-' + n) as arq:
+with open('./res/' + n + '/res-nsgal-' + n) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
