1f6030c41c4036bdb6330f6c58f37a57ac63bae1
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-03-25 15:20:08 -0300

    anova completo

--- delta/anova_2_fatores.py
+++ delta/anova_2_fatores.py
@@ -27,10 +27,11 @@ model = ols(formula, data).fit()
 aov_table = statsmodels.stats.anova.anova_lm(model, typ = 2)
 print(aov_table)
 
-#                                   sum_sq     df           F       p-valor
-# Algoritmo                 	  0.000019    3.0  200.972122  1.298555e-75
-# Cardinalidade               	  0.000003    2.0   50.828452  4.299798e-20
-# Interacao					 	  0.000005    6.0   26.454463  6.227172e-26
-# Erro                       	  0.000011  348.0
+#                                   	SS     df  			MS         			 F       p-valor
+# Algoritmo                 	  0.000019    3.0  		6.33e-06		200.972122  1.298555e-75
+# Cardinalidade               	  0.000003    2.0   	1.50e-06		50.828452   4.299798e-20
+# Interacao					 	  0.000005    6.0   	8.33e-07		26.454463   6.227172e-26
+# Erro                       	  0.000011  348.0		3.160920e-08
 
+# MS = SS/df, pra cada linha
 # Resultado significante tanto para cardinalidade quanto para algoritmo, com nivel de confianca p < 0.05.
--- hv/anova_2_fatores.py
+++ hv/anova_2_fatores.py
@@ -27,10 +27,11 @@ model = ols(formula, data).fit()
 aov_table = statsmodels.stats.anova.anova_lm(model, typ = 2)
 print(aov_table)
 
-#                                  	sum_sq       df           F        p-valor
-# Algoritmo                  		0.000654    3.0   14.012397   1.208370e-08
-# Cardinalidade             		0.015055    2.0  484.013436  3.038930e-101
-# Interacao  						0.000342    6.0    3.660914   1.540434e-03
-# Erro                       		0.005412  348.0
+#                                  		  SS     df			   MS	           F        p-valor
+# Algoritmo                  		0.000654    3.0		0.0002179	  14.012397    1.208370e-08
+# Cardinalidade             		0.015055    2.0		0.0075275	  484.013436  3.038930e-101
+# Interacao  						0.000342    6.0		0.0000057 	  3.660914     1.540434e-03
+# Erro                       		0.005412  348.0		0.0000016
 
+# MS = SS/df, pra cada linha
 # Resultado significante tanto para cardinalidade quanto para algoritmo, com nivel de confianca p < 0.05.
