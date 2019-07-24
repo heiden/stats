6e3dec5b5292f9510e59b30f0a11b0839e9c1ce4
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-07-24 12:21:03 -0300

    graficos sem margens

--- grafico.py
+++ grafico.py
@@ -4,6 +4,14 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
+lol
+lol
+lol
+lol
+lol
+lol
+lol
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 titulo = 'test'
