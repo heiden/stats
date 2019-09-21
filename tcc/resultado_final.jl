e582c178e2f96a5da41a5d51cb53844673b27d07
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-09-21 20:51:40 -0300

    shitty tests... weird

--- tcc/resultado_final.jl
+++ tcc/resultado_final.jl
@@ -9,9 +9,8 @@ function plot(fronteira, dir, arq)
 	end
 end
 
-k = ["3", "9", "15"]
-# algs = ["brkga", "hibrido", "nsga"]
-algs = ["hibrido"]
+k = ["3"]
+algs = ["brkga"]
 
 for alg in algs
 	for i in k
