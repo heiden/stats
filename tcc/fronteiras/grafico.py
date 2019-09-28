fc7076f06ae305440d74aa75e39968eed7a51083
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-09-28 18:23:09 -0300

    testes pra busca local

--- tcc/fronteiras/grafico.py
+++ tcc/fronteiras/grafico.py
@@ -13,61 +13,61 @@ ax.set_xlabel('Risco')
 ax.set_ylabel('Retorno')
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
-with open('brkga-{0}'.format(n)) as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
-
-ax.plot(x, y, 'x', c = '#c61f7b', label = u'BRKGA Original 3')
-
-with open('sem-params/brkga-{0}'.format(n)) as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
-
-ax.plot(x, y, 'x', c = '#1fc6be', label = u'BRKGA Parametrizado 3')
-
-with open('brkga-9'.format(n)) as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
-
-ax.plot(x, y, 'x', c = '#000000', label = u'BRKGA Original 9')
-
-with open('sem-params/brkga-9'.format(n)) as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
-
-ax.plot(x, y, 'x', c = '#93f413', label = u'BRKGA Parametrizado 9')
-
-with open('brkga-15'.format(n)) as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
-
-ax.plot(x, y, 'x', c = '#e0e04c', label = u'BRKGA Original 15')
-
-with open('sem-params/brkga-15'.format(n)) as arq:
-	linhas = arq.readlines()
-	x = [float(linha.split()[0]) for linha in linhas]
-	y = [float(linha.split()[1]) for linha in linhas]
-
-ax.plot(x, y, 'x', c = '#e04c4c', label = u'BRKGA Parametrizado 15')
-
-# with open('nsga-{0}'.format(n)) as arq:
+# with open('brkga-{0}'.format(n)) as arq:
 # 	linhas = arq.readlines()
 # 	x = [float(linha.split()[0]) for linha in linhas]
 # 	y = [float(linha.split()[1]) for linha in linhas]
 
-# ax.plot(x, y, 'x', c = '#93f413', label = 'NSGA')
+# ax.plot(x, y, 'x', c = '#c61f7b', label = u'BRKGA Original 3')
 
-# with open('hibrido-{0}'.format(n)) as arq:
+# with open('sem-params/brkga-{0}'.format(n)) as arq:
 # 	linhas = arq.readlines()
 # 	x = [float(linha.split()[0]) for linha in linhas]
 # 	y = [float(linha.split()[1]) for linha in linhas]
 
-# ax.plot(x, y, 'x', c = '#1fc6be', label = u'Híbrido')
+# ax.plot(x, y, 'x', c = '#1fc6be', label = u'BRKGA Parametrizado 3')
+
+# with open('brkga-9'.format(n)) as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
+
+# ax.plot(x, y, 'x', c = '#000000', label = u'BRKGA Original 9')
+
+# with open('sem-params/brkga-9'.format(n)) as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
+
+# ax.plot(x, y, 'x', c = '#93f413', label = u'BRKGA Parametrizado 9')
+
+# with open('brkga-15'.format(n)) as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
+
+# ax.plot(x, y, 'x', c = '#e0e04c', label = u'BRKGA Original 15')
+
+# with open('sem-params/brkga-15'.format(n)) as arq:
+# 	linhas = arq.readlines()
+# 	x = [float(linha.split()[0]) for linha in linhas]
+# 	y = [float(linha.split()[1]) for linha in linhas]
+
+# ax.plot(x, y, 'x', c = '#e04c4c', label = u'BRKGA Parametrizado 15')
+
+with open('nsga-{0}'.format(n)) as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
+
+ax.plot(x, y, 'x', c = '#93f413', label = 'NSGA')
+
+with open('pontos66'.format(n)) as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
+
+ax.plot(x, y, 'x', c = '#1fc6be', label = u'Híbrido')
 
 leg = ax.legend(loc = 4)
 
