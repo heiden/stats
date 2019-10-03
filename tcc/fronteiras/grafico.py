e3ca39173354c0b44623612b1551d559a0153e7e
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-10-02 23:28:30 -0300

    versus?

--- tcc/fronteiras/grafico.py
+++ tcc/fronteiras/grafico.py
@@ -14,21 +14,49 @@ ax.set_xlabel('Risco')
 ax.set_ylabel('Retorno')
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
-with open('../dados-com-params/{0}-{1}'.format(alg, n)) as arq:
+with open('../dados-sem-params/{0}-3'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#ff9114', label = u'Com Parametrização Adaptativa')
+ax.plot(x, y, 'x', c = '#ed17cd', label = u'3')
 
-with open('../dados-sem-params/{0}-{1}'.format(alg, n)) as arq:
+with open('../dados-com-params/{0}-3'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#ed17cd', label = u'Sem Parametrização Adaptativa')
+ax.plot(x, y, 'x', c = '#ff9114', label = u'3*')
 
-# with open('hibrido-{0}'.format(n)) as arq:
+with open('../dados-sem-params/{0}-9'.format(alg)) as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
+
+ax.plot(x, y, 'x', c = '#dbca0d', label = u'9')
+
+with open('../dados-com-params/{0}-9'.format(alg)) as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
+
+ax.plot(x, y, 'x', c = '#34d8c5', label = u'9*')
+
+with open('../dados-sem-params/{0}-15'.format(alg)) as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
+
+ax.plot(x, y, 'x', c = '#ca0ddb', label = u'15')
+
+with open('../dados-com-params/{0}-15'.format(alg)) as arq:
+	linhas = arq.readlines()
+	x = [float(linha.split()[0]) for linha in linhas]
+	y = [float(linha.split()[1]) for linha in linhas]
+
+ax.plot(x, y, 'x', c = '#1edb0d', label = u'15*')
+
+# with open('hibrido-{0}''.format(n)') as arq:
 # 	linhas = arq.readlines()
 # 	x = [float(linha.split()[0]) for linha in linhas]
 # 	y = [float(linha.split()[1]) for linha in linhas]
@@ -38,4 +66,4 @@ ax.plot(x, y, 'x', c = '#ed17cd', label = u'Sem Parametrização Adaptativa')
 leg = ax.legend(loc = 4)
 
 # plt.show()
-plt.savefig(titulo + '-com-busca-local.png')
+plt.savefig(titulo + '-versus.png')
