c873bc281786481773b447cbe860c87e0a1bbec3
Alexandre Heiden <alexandreheiden@hotmail.com>
2019-10-11 11:55:29 -0300

    c pra busca vs nao busca + graficos

--- tcc/fronteiras/grafico-vs.py
+++ tcc/fronteiras/grafico-vs.py
@@ -13,42 +13,42 @@ ax.set_xlabel('Risco')
 ax.set_ylabel('Retorno')
 ax.yaxis.set_major_formatter(FormatStrFormatter('%.4f'))
 
-with open('../dados-sem-params/{0}-3'.format(alg)) as arq:
+with open('{0}-3'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#ed17cd', label = u'3')
+ax.plot(x, y, 'x', c = '#ed17cd', label = u'3-BL')
 
-with open('../dados-com-params/{0}-3'.format(alg)) as arq:
+with open('{0}-3-params'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#ff9114', label = u'3*')
 
-with open('../dados-sem-params/{0}-9'.format(alg)) as arq:
+with open('{0}-9'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#dbca0d', label = u'9')
+ax.plot(x, y, 'x', c = '#dbca0d', label = u'9-BL')
 
-with open('../dados-com-params/{0}-9'.format(alg)) as arq:
+with open('{0}-9-params'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
 ax.plot(x, y, 'x', c = '#34d8c5', label = u'9*')
 
-with open('../dados-sem-params/{0}-15'.format(alg)) as arq:
+with open('{0}-15'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
 
-ax.plot(x, y, 'x', c = '#ca0ddb', label = u'15')
+ax.plot(x, y, 'x', c = '#ca0ddb', label = u'15-BL')
 
-with open('../dados-com-params/{0}-15'.format(alg)) as arq:
+with open('{0}-15-params'.format(alg)) as arq:
 	linhas = arq.readlines()
 	x = [float(linha.split()[0]) for linha in linhas]
 	y = [float(linha.split()[1]) for linha in linhas]
